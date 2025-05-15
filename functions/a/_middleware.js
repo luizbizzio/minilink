79830b892ff7c94da321cf4d91a6545ef01fb645
Luiz Bizzio <contact@luizbizzio.com>
2025-05-15 03:40:00 +0200

    Delete functions/a directory

--- functions/a/_middleware.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest({ request, next }) {
-  // let static handle "/", "/admin/" and anything under "/admin/"
-  const p = new URL(request.url).pathname;
-  if (p === '/' || p.startsWith('/admin/')) {
-    return next();
-  }
-  // otherwise, fall back to the slug handler
-  return next();
-}
